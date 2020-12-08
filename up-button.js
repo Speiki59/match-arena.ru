f7a4027ba495827a22f5ec376b4f8d5eb2e59edd
Artemichi <Artemchugainov@gmail.com>
2020-12-09 01:22:13 +0500

    nemnogo helpi dlya speiki scandalista

--- up-button.js
+++ up-button.js
@@ -1,14 +1,31 @@
-let upButton = document.querySelector('.up-button');
+let upButton = document.querySelector(".up-button");
 
+// хз мне чет лень писать как работать обработчик анимации так что просто потом спросишь что да как
 window.onscroll = function () {
   if (window.pageYOffset > 200) {
-    upButton.classList.add('shown');
+    if (upButton.classList.contains("up-button__hide")) {
+      upButton.classList.remove("up-button__hide");
+    }
+    upButton.classList.add("up-button__show");
   } else {
-    upButton.classList.remove('shown');
+    if (upButton.classList.contains("up-button__show")) {
+      upButton.classList.remove("up-button__show");
+      upButton.classList.add("up-button__hide");
+    }
   }
-
+};
+const scrollToTop = () => {
+  const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, currentScroll - currentScroll / 20);
+  }
+};
+upButton.onclick = () => {
+  scrollToTop();
 };
 
-upButton.onclick = function () {
-  window.scrollTo(0, 0);
-};
+// ВЫБОР ГОРОДА
+const currentCity = document.querySelector(".city​-selection");
+currentCity.innerHTML =
+  window.location.href.split("/").pop().split(".").shift() === "prm" ? "пермь" : "екатеринбург";
